612dca03ad8123639bab74650a87e745b4672a58
Enrico Ros <enrico.ros@gmail.com>
2021-01-24 23:20:58 -0800

    ui: cleanup, add typescript support, add an icon/identity

---
+++ web-ui/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
