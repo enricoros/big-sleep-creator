fe95d8a1ba79622cadd5522df0133d4699619403
Enrico Ros <enrico.ros@gmail.com>
2021-01-25 01:11:06 -0800

    ui: modularize UI, prepare for data binding

--- web-ui/src/App.tsx
+++ web-ui/src/App.tsx
@@ -1,18 +1,27 @@
 import React from "react";
 import {
-  AppBar,
+  AppBar, Box,
   Button,
   Card,
   CardActions,
   CardContent,
-  CardMedia,
+  CardMedia, CircularProgress,
   Container,
-  Grid,
-  makeStyles,
+  Grid, Icon, IconButton,
+  makeStyles, Paper, TextField,
   Toolbar,
   Typography
 } from "@material-ui/core";
+
 import BrushIcon from '@material-ui/icons/Brush';
+import EditIcon from '@material-ui/icons/Edit';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import PauseCircleFilledIcon from '@material-ui/icons/PauseCircleFilled';
+import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import StopIcon from '@material-ui/icons/Stop';
+
 import {siteTitle} from "./brand";
 
 const useStyles = makeStyles((theme) => ({
@@ -22,6 +31,9 @@ const useStyles = makeStyles((theme) => ({
   heroContent: {
     backgroundColor: theme.palette.background.paper,
     padding: theme.spacing(8, 0, 6),
+  },
+  heroHyperParams: {
+    padding: theme.spacing(1, 1.5),
   },
   experimentsGrid: {
     paddingTop: theme.spacing(8),
@@ -44,79 +56,164 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const topBar = props =>
-  <AppBar position="relative">
-  <Toolbar>
-    <BrushIcon className={props.classes.icon}/>
-    <Typography variant="h6" color="inherit" noWrap>
-      {siteTitle}
-    </Typography>
-  </Toolbar>
-</AppBar>;
 
-function App() {
+function TopBar(props) {
+  // TODO: subscribe to overall status
+  const [connected, setConnected] = React.useState(true);
+  const [busy, setBusy] = React.useState(false);
+  return <AppBar position="relative">
+    <Toolbar>
+      <BrushIcon className={props.classes.icon}/>
+      <Typography variant="h6" color="inherit" noWrap>
+        {siteTitle}
+      </Typography>
+      <Box flexGrow={1}/>
+      <Box display="flex" flexDirection="row" alignItems="center" alignContent="middle">
+        <Typography variant="h6" color="inherit" noWrap onClick={() => {
+          setBusy(!busy);
+          setConnected(!connected);
+        }}>
+          Status: {connected ? <>connected, {busy ? <b style={{color: 'lightpink'}}>busy</b> : <span style={{color: 'lightgreen'}}>ready</span>}</>
+          : <b style={{color: 'darkred'}}>disconnected</b>}
+        </Typography>
+        {busy && <CircularProgress color="secondary" size="2rem" style={{marginLeft: '1em'}}/>}
+      </Box>
+    </Toolbar>
+  </AppBar>;
+}
+
+function NewInput(props) {
+  const [showHyper, setShowHyper] = React.useState<boolean>(false);
+
+  const [nextText, setNextText] = React.useState<string>('');
+
+  function pressedEnter() {
+    console.log('DO IT!');
+  }
+
+  return <div className={props.classes.heroContent}>
+    <Container component="main" maxWidth="lg">
+      <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+        Let's dream along
+      </Typography>
+
+      <Box display="flex" flexDirection="column">
+        {/* Input field, with related actions*/}
+        <Box display="flex" flexDirection="row">
+          <TextField id="newText" name="newText" label="Describe the image" placeholder="a puppy with blue eyes"
+                     variant="outlined" margin="normal" fullWidth value={nextText} onChange={t => setNextText(t.target.value)}
+                     onKeyPress={ev => {
+                       if (ev.key === 'Enter') {
+                         ev.preventDefault();
+                         pressedEnter();
+                       }
+                     }}>
+          </TextField>
+          <IconButton size="medium" onClick={() => setShowHyper(!showHyper)}>
+            {showHyper ? <ExpandLessIcon fontSize="large"/> : <ExpandMoreIcon fontSize="large"/>}
+          </IconButton>
+          <IconButton size="medium">
+            <PlayArrowIcon fontSize="large"/>
+          </IconButton>
+        </Box>
+        {/* Hyper parameters show/edit box */}
+        {showHyper &&
+        <Paper className={props.classes.heroHyperParams}>
+          <Box>
+            <Typography variant={"subtitle2"} color="textSecondary">
+              Hyper parameters for a new generation
+            </Typography>
+            <Box fontFamily="monospace" style={{marginLeft: '1em'}}>
+              <Typography>
+                text={JSON.stringify(nextText, null, 2)}
+              </Typography>
+            </Box>
+          </Box>
+          <CardActions>
+            <Button size="small" color="primary" disabled={true}>
+              <EditIcon fontSize={"small"} className={props.classes.icon}/>
+              Edit
+            </Button>
+          </CardActions>
+        </Paper>}
+      </Box>
+
+      <Typography variant="h6" align="center" color="textSecondary" paragraph>
+        This is a work in progress.
+      </Typography>
+    </Container>
+  </div>;
+}
+
+function ExperimentCard(props) {
+  const imageUrl = 'https://source.unsplash.com/random';
+  const imageTitle = 'test1';
+  return <Card className={props.classes.experiment}>
+    <CardMedia className={props.classes.experimentImage} image={imageUrl} title={imageTitle}>
+      <Box display="flex" flexDirection="row">
+        <Box flexGrow={1}/>
+        <Box style={{backgroundColor: '#000A'}}>
+          <IconButton size="medium">
+            <PlayCircleFilledIcon fontSize="large" color="primary"/>
+          </IconButton>
+          <IconButton size="medium">
+            <PauseCircleFilledIcon fontSize="large" color="secondary"/>
+          </IconButton>
+          <IconButton size="medium" disabled={true}>
+            <StopIcon fontSize="large" color="error"/>
+          </IconButton>
+        </Box>
+      </Box>
+    </CardMedia>
+    <CardContent className={props.classes.experimentContent}>
+      <Typography gutterBottom variant="body1">
+        Generated on {(new Date()).toLocaleString()}.
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <Button size="small" color="primary">View</Button>
+      <Button size="small" color="primary">Save</Button>
+      <Button size="small" color="primary">Edit</Button>
+    </CardActions>
+  </Card>;
+}
+
+function Experiments(props) {
+  return <Container className={props.classes.experimentsGrid} maxWidth="md">
+    <Grid container spacing={4}>
+      <Grid item xs={12} sm={6} md={4}>
+        <ExperimentCard classes={props.classes}/>
+      </Grid>
+    </Grid>
+  </Container>;
+}
+
+function Footer(props) {
+  return <footer className={props.classes.footer}>
+    <Typography variant="h6" align="center" gutterBottom>
+      This is a footer
+    </Typography>
+    <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+      Tell me the purpose of this footer
+    </Typography>
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  </footer>;
+}
+
+export function App() {
   const classes = useStyles();
   return (
     <React.Fragment>
-      <topBar classes={classes}/>
+      <TopBar classes={classes}/>
       <main>
-        {/* Title */}
-        <div className={classes.heroContent}>
-          <Container component="main" maxWidth="lg">
-            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-              {siteTitle}
-            </Typography>
-            <Typography variant="h5" align="center" color="textSecondary" paragraph>
-              This is a work in progress.
-            </Typography>
-          </Container>
-        </div>
-
-        {/* Output */}
-        <Container className={classes.experimentsGrid} maxWidth="md">
-          <Grid container spacing={4}>
-            <Grid item key={'1'} xs={12} sm={6} md={4}>
-              <Card className={classes.experiment}>
-                <CardMedia
-                  className={classes.experimentImage}
-                  image="https://source.unsplash.com/random"
-                  title="Test1"
-                />
-                <CardContent className={classes.experimentContent}>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    Heading
-                  </Typography>
-                  <Typography>
-                    This is a media card. You can use this section to describe the content.
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button size="small" color="primary">View</Button>
-                  <Button size="small" color="primary">Edit</Button>
-                </CardActions>
-              </Card>
-            </Grid>
-          </Grid>
-        </Container>
+        <NewInput classes={classes}/>
+        <Experiments classes={classes}/>
       </main>
-
-      {/* Footer */}
-      <footer className={classes.footer}>
-        <Typography variant="h6" align="center" gutterBottom>
-          This is a footer
-        </Typography>
-        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
-          Tell me the purpose of this footer
-        </Typography>
-        <Typography variant="body2" color="textSecondary" align="center">
-          {'Copyright © '}
-          {new Date().getFullYear()}
-          {'.'}
-        </Typography>
-      </footer>
+      <Footer classes={classes}/>
     </React.Fragment>
   );
 }
-
-export default App;
-
--- web-ui/src/index.js
+++ web-ui/src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import {ThemeProvider, createMuiTheme} from '@material-ui/core/styles';
-import App from './App';
+import {App} from './App';
 
 const theme = createMuiTheme({
   palette: {
     primary: {
       main: '#3B88C3',
-    },
+    },/*
     secondary: {
       main: '#C1694F',
-    },
+    },*/
     background: {
       default: '#fff',
     },
