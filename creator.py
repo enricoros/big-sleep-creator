c325b4fd1e2bcf94d76fb6e36c16aa1978c857da
Enrico Ros <enrico.ros@gmail.com>
2021-01-24 21:01:12 -0800

    Testing socket.io/flask

--- creator.py
+++ creator.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
 from big_sleep import Imagine
 import fire
 import torch
@@ -37,13 +37,30 @@ def run_app(http_host=default_http_address, http_port=default_http_port):
     print('# Starting HTTP endpoint on ' + http_host + ':' + str(http_port))
     app = Flask(__name__)
     app.logger.setLevel(20)
+    socket_io = SocketIO(app)
     print()
 
     # test out app functionality at start
-    initial_test()
+    # initial_test()
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    @socket_io.on('my event')
+    def handle_my_custom_event(json):
+        print('received json: ' + str(json))
+
+    @socket_io.on('connect')
+    def test_connect():
+        print('Client connected')
+        emit('my response', {'data': 'Connected'})
+
+    @socket_io.on('disconnect')
+    def test_disconnect():
+        print('Client disconnected')
 
     # start sever
-    socket_io = SocketIO(app)
     socket_io.run(app, host=http_host, port=http_port)
 
 
